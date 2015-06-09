3f65928e5235f2aa1a193ff5a05a1cd9cee7fe35
Jason Tan <j-tan@outlook.com>
2015-06-10 08:47:08 +1000

    Added argument handling
    Argument handling functionality has been implemented, providing entityid
    variable assignment as well as handling unknown arguments

--- mdtester.sh
+++ mdtester.sh
@@ -10,3 +10,17 @@ if [ "$#" -gt 2 ]; then
   usage
   exit 1
 fi
+
+while [ "$1" != "" ]; do
+  case $1 in
+    --entityid )
+      shift
+      entityid="$1"
+      ;;
+    * )
+      printf "Unknown argument '$1'\n"
+      usage
+      exit 1
+      ;;
+  esac
+done
