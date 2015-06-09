74e91b07beab04da3afdd0af13d53bf41e05f800
Jason Tan <j-tan@outlook.com>
2015-06-10 08:30:19 +1000

    Zero argument invocation now implemented
    Zero argument invocations should no longer trigger usage message.

--- mdtester.sh
+++ mdtester.sh
@@ -3,6 +3,10 @@
 entityid="https://test-sp.test.aaf.edu.au/idp/shibboleth"
 
 usage() {
-  printf "Usage: mdtester.sh [--entityid ENTITYID]"
+  printf "Usage: mdtester.sh [--entityid ENTITYID]\n"
 }
 
+if [ "$#" -gt 2 ]; then
+  usage
+  exit 1
+fi
