c542c33d13a381dc4b394c43a1ccc26f4df9d9fa
Jason Tan <j-tan@outlook.com>
2015-06-10 11:55:27 +1000

    Each url will now be attempted to be accessed
    Each URL produced by the script is now fed into curl utility in order to
    test it. The output should be displayed on the console

--- mdtester.sh
+++ mdtester.sh
@@ -40,4 +40,5 @@ grep DiscoveryResponse ./AAF-metadata.xml | awk -F"Location=" '{print $2}' \
   else
     url="$(sed 's:/[^/]*$::' <<< "$line")/Login?entityID=$entityid"
   fi
+  curl -skL -w "%{http_code} %{url_effective}\\n" "$url" -o /dev/null
 done
